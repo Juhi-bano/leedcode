09a2eeaad78fd3ec4dcc19cafa625a4aa6fffc6d
Juhi-bano <juhibano783@gmail.com>
2026-04-25 10:32:53 +0530

    Time: 0 ms (100%), Space: 19.6 MB (7.95%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,6 +1,6 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        output = []
+        '''output = [] # here we are use brouteforce method not solve using stack
         result = []
         for i in range(len(nums2)):
             ans = -1
@@ -13,5 +13,24 @@ class Solution:
         for i in nums1:
             index = mapped[i]
             output.append(result[index])
-        return output
+        return output'''
         
+
+        stack = []
+        result = []
+        for i in range(len(nums2)-1,-1,-1):
+            while (len(stack)>0 and nums2[i] >= stack[-1]):
+                stack.pop()
+            if len(stack) == 0:
+                result.append(-1)
+                stack.append(nums2[i])
+            else:
+                result.append(stack[-1])
+                stack.append(nums2[i])
+        result = result[::-1]
+        output = []
+        mapped = {nums2[i]:i for i in range(len(nums2))}
+        for i in nums1:
+            index = mapped[i]
+            output.append(result[index])
+        return output
