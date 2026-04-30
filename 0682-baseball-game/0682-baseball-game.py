58ad5c21dde6b0b0bc0d1b2af2063517d6899815
Juhi-bano <juhibano783@gmail.com>
2026-04-30 22:59:06 +0530

    Time: 0 ms (100%), Space: 19.4 MB (44.33%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -1,15 +1,15 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         result = []
-        for op in range(len(operations)):
-            if operations[op] == "C":
+        for op in range(len(operations)): # for op in operations:
+            if operations[op] == "C": #if op == "C"
                 if result:
                     result.pop()
-            elif operations[op] == "D":
+            elif operations[op] == "D": # elif op == "D":
                 result.append(2*result[-1])
             elif operations[op] == "+":
                 result.append(result[-1]+result[-2])
             else:
-                result.append(int(operations[op]))
+                result.append(int(operations[op])) # result.append(int(op))
         return sum(result)
         
