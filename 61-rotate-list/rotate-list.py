7816dde4c829af1e2b866d2b1b25c75ac41032de
Juhi-bano <juhibano783@gmail.com>
2026-06-13 09:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.54%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -14,18 +14,13 @@ class Solution:
             tail = tail.next
             length += 1
 
-        k = k % length 
+        k = k % length
         if k == 0:
             return head
-        new_tail = head        
+        new_tail = head
         for i in range(length - k - 1):
             new_tail = new_tail.next
         new_head = new_tail.next
         tail.next = head
         new_tail.next = None
         return new_head
-
-
-        
-
-        
