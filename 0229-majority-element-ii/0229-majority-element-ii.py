0efca77aacb952a4b4175ff71a90e56ad6b55489
Juhi-bano <juhibano783@gmail.com>
2026-06-19 14:32:10 +0530

    Time: 6 ms (70.96%), Space: 23.5 MB (5.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,14 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        appears = floor(n/3)
         freq = {}
         result = []
-        appear = (n/3)
         for num in nums:
             freq[num] = freq.get(num,0)+1
         for key in freq:
-            if freq[key] > appear:
+            if freq[key] > appears:
                 result.append(key)
         return result
 
-        
+
+    
+
+       
