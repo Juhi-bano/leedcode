968233ac5ea13badb10cf0b145911b2f0a306c70
Juhi-bano <juhibano783@gmail.com>
2026-06-19 10:04:32 +0530

    Time: 4 ms (2.08%) | Memory: 19.3 MB (52.19%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        result = [0]
+        for i in gain:
+            result.append(result[-1]+i)
+        return max(result)
+        
