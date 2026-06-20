3342828e333076939a30abc12f543aa427da96f7
Juhi-bano <juhibano783@gmail.com>
2026-06-20 18:04:21 +0530

    Time: 619 ms (11.59%), Space: 19.4 MB (54.06%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,21 +1,47 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        if not nums or len(nums)<4:
+        #bruade force method hai
+        '''n = len(nums)
+        my_set = set()
+        for i in range(0,n):
+            for j in range(i+1,n):
+                for k in range(j+1,n):
+                    for l in range(k+1,n):
+                        if nums[i] + nums[j] + nums[k] + nums[l] == target:
+                            temp = [nums[i],nums[j],nums[k],nums[l]]
+                            temp.sort()
+                            my_set.add(tuple(temp))
+        return [list(ans) for ans in my_set]'''
+
+        n = len(nums)
+        result = set()
+        nums.sort()
+        if not nums or n < 4:
             return []
-        nums = sorted(nums)
-        ans = set()
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                start = j+1
-                end = len(nums)-1
+        for i in range(0,n):
+            for j in range(i+1,n):
+                start = j + 1
+                end = n - 1
                 while start < end:
-                    current = nums[i] + nums[j] + nums[start] + nums[end]
-                    if current == target:
-                        ans.add((nums[i],nums[j],nums[start],nums[end]))
+                    Total = nums[i] + nums[j] + nums[start] + nums[end]
+                    if Total == target:
+                        temp = [nums[i],nums[j],nums[start],nums[end]]
+                        result.add(tuple(temp))
                         start += 1
                         end -= 1
-                    elif current < target:
+                    elif Total < target:
                         start += 1
-                    elif current > target:
+                    elif Total > target:
                         end -= 1
-        return list(ans)
+        return [list(ans) for ans in result]
+                
+
+
+       
+
+
+        
+
+
+        
+      
